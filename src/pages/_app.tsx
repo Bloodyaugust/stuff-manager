e7298ea5ff0cf7f8c97795950f5c229093488b5b
Bloodyaugust <greysonrichey@gmail.com>
2023-06-26 21:29:34 -0600

    Add ErrorBoundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,26 +4,29 @@ import { SessionProvider } from 'next-auth/react';
 import UserProvider from '@/components/UserProvider';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@/lib/query';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'dark',
-      }}
-    >
-      <SessionProvider session={session}>
-        <UserProvider>
-          <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-          </QueryClientProvider>
-        </UserProvider>
-      </SessionProvider>
-    </MantineProvider>
+    <ErrorBoundary>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <SessionProvider session={session}>
+          <UserProvider>
+            <QueryClientProvider client={queryClient}>
+              <Component {...pageProps} />
+            </QueryClientProvider>
+          </UserProvider>
+        </SessionProvider>
+      </MantineProvider>
+    </ErrorBoundary>
   );
 }
